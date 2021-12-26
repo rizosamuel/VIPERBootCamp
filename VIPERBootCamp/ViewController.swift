e1d474a4b97a595d7be9d91a754c5f6dd93b1164
Rijo <rizosamuel1995@gmail.com>
2021-12-26 18:23:02 +0530

    Initial Commit

---
+++ VIPERBootCamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPERBootCamp
+//
+//  Created by Rijo Samuel on 26/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
